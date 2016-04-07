c5c897a466b56a9a7daf1b7cbf0b6bd613759247
Shannon <shaw0106@algonquinlive.com>
2016-04-07 08:16:31 -0400

    set vars

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+var $body = $(.banner);
+var $stars;
+var banHeight = $body.outWidth();
+var banWidth = $body.outHeight();
+
+
+
+
+
+
 // Number of stars: 75
 // Randomized: top, left, opacity, transform: rotate() scale()
 // Getting the width/height of elements: outerWidth(), outerHeight()
