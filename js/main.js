53db622750fde20404c3cc5644326e614cd1f7ed
Shannon <shaw0106@algonquinlive.com>
2016-04-07 08:20:42 -0400

    not working

--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@ var $stars;
 var banHeight = $body.outWidth();
 var banWidth = $body.outHeight();
 
+for (i = 0; i < 75; i++) {
+  $star = $('<div>');
+  $star.addClass('star');
+  $star.css({
+    top: Mathrandom() * bannerHeight;
+    left: Mathrandom() * bannerWidth;
+  })
+}
 
 
 
@@ -11,3 +19,5 @@ var banWidth = $body.outHeight();
 // Number of stars: 75
 // Randomized: top, left, opacity, transform: rotate() scale()
 // Getting the width/height of elements: outerWidth(), outerHeight()
+// transform-origin, animation, alternate, infinate, @keyframes
+//
